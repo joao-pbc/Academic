442a973ff1409aac17becaf7b4888a249c8f1183
João Pedro Bove da Costa <88252025+joao-pbc@users.noreply.github.com>
2025-10-31 16:51:14 -0300

    Delete Code directory

--- Code/Mult.java
+++
@@ -1,9 +0,0 @@
-package Code;
-
-public class Mult {
-	public static double mult(double num1, double num2) {
-		double numero1 = num1, numero2 = num2;
-		return numero1 * numero2;
-		
-	}
-}
--- Code/Sub.java
+++
@@ -1,9 +0,0 @@
-package Code;
-
-public class Sub {
-	public static double sub(double num1, double num2) {
-		double numero1 = num1, numero2 = num2;
-		return numero1 - numero2;
-		
-	}
-}
